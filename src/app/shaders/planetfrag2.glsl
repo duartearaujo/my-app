41d6f3d0ae778408c493c93bb2838c3b85ed227a
a100750 <duarte.soares.araujo@gmail.com>
2025-07-23 02:55:14 +0100

    changed another pattern

--- src/app/shaders/planetfrag2.glsl
+++ src/app/shaders/planetfrag2.glsl
@@ -9,7 +9,7 @@ void main () {
     float spacing = 0.5;
     float thickness = 0.5;
 
-    float wave = sin(vPosition.x * frequency) * amplitude;
+    float wave = sin(vPosition.x * frequency + vPosition.z) * amplitude;
     float line = step(0.5, mod(vPosition.y + wave, 1.0));
     vec3 fragColor = mix(vec3(0.0, 1.0, 0.6667), vec3(0.0118, 0.2196, 0.2471), line);
     csm_DiffuseColor.rgb = vec3(fragColor);
